0e68973d1564d7d7d201708c9da1cd2d14b11d85
moradi <h.moradi@matlabsite.com>
2022-05-05 13:10:57 -0700

    login system

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,37 +4,57 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
 
     use AuthenticatesUsers;
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = RouteServiceProvider::HOME;
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    public function attemptLogin(Request $request)
     {
-        $this->middleware('guest')->except('logout');
+        $token = $this->guard()->attempt($this->credentials($request));
+        if (! $token){
+            return false;
+        }
+
+        $user = $this->guard()->user();
+
+        if ($user instanceof MustVerifyEmail && ! $user->hasVerifiedEmail()){
+            return false;
+        }
+
+        $this->guard()->setToken($token);
+        return true;
+    }
+
+    protected function sendLoginResponse(Request $request)
+    {
+        $this->clearLoginAttempts($request);
+        $token = (string)$this->guard()->getToken();
+        $exiration = $this->guard()->getPayload()->get('exp');
+
+        return response()->json([
+            'token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => $exiration
+        ]);
+    }
+
+    protected function sendFailedLoginResponse(Request $request)
+    {
+        $user = $this->guard()->user();
+
+        if ($user instanceof MustVerifyEmail && ! $user->hasVerifiedEmail()){
+            return response()->json(['errors' => [
+                'verification' => 'شما باید ایمیل خود را فعال کنید'
+            ]]);
+        }
+        throw ValidationException::withMessages([
+            $this->username() => 'ایمیل یا پسورد وارد شده نامعتبر است'
+        ]);
     }
 }
