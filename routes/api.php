45aae8209c173e72eada81f196232c041e852bd1
ho3inmoradiii <h.moradi@matlabsite.com>
2022-05-13 11:51:23 -0700

    commit

--- routes/api.php
+++ routes/api.php
@@ -9,8 +9,6 @@ Route::group(['middleware' => ['auth:api']],function (){
     Route::post('logout','Auth\LoginController@logout');
 });
 
-
-
 Route::group(['middleware'=>['guest:api']],function (){
     Route::post('register','Auth\RegisterController@register');
     Route::post('verification/verify/{user}','Auth\VerificationController@verify')->name('verification.verify');
