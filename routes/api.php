d55a516e14f32cfc0bee2ff76af17afeddb737d1
ho3inmoradiii <ho3inmoradi1998@gmail.com>
2022-07-30 02:34:02 -0700

    edit

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+//Route::middleware(['cors'])->group(function () {
+//
+//});
 
 Route::get('me','Auth\LoginController@getMe');
 
